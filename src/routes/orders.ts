d65cb77e88893c9184241b8b2324df0c4eaedbb3
munene-m <munenenjue18@gmail.com>
2024-05-16 12:42:14 +0300

    deleted files

--- src/routes/orders.ts
+++
@@ -1,9 +0,0 @@
-import { initiateOrder } from "../controllers/orders";
-import express from "express";
-import authProtect from "../middleware/authMiddleware";
-
-const router = express.Router();
-
-router.route("/initiate").post(authProtect, initiateOrder);
-
-export default router;
