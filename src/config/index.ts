ea758d4962e898e2f4a33e1f9e598d0e0637aa92
munene-m <munenenjue18@gmail.com>
2024-05-19 14:18:53 +0300

    provide alternative port

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const config = {
-  port: process.env.PORT,
+  port: process.env.PORT || 8080,
   prefix: process.env.API_PREFIX,
   databaseUri: process.env.MONGO_URI,
 };
