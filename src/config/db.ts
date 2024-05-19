3fce46d4a0d346d120b8bdce66a2909d8de3d602
munene-m <munenenjue18@gmail.com>
2024-05-19 14:09:19 +0300

    use db uri from config file

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
+import config from "./index";
 export async function connectDB() {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL || "");
+    const conn = await mongoose.connect(config.databaseUri || "");
     console.log("MongoDB connected successfully");
   } catch (err: any) {
     console.log(err.message);
